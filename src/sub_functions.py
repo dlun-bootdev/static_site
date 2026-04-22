c573289eb41f19b83afe03d20b6fd939784c8987
ThePrimeagen <the.primeagen@aol.com>
2026-04-22 17:17:11 +0800

    more sub fun

--- src/sub_functions.py
+++ src/sub_functions.py
@@ -2,7 +2,6 @@ import re
 from textnode import TextNode, TextType
 
 
-
 def extract_markdown_images(text):
     return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
@@ -28,3 +27,53 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_nodes.append(TextNode(split, text_type))
                 text = True
     return new_nodes
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        image_list = extract_markdown_images(node.text)
+
+        if len(image_list) == 0:
+            new_nodes.append(TextNode(node.text, node.text_type))
+            continue
+        
+        sections = []
+        left_over_text = node.text
+        for alt, url in image_list:
+            sections = left_over_text.split(f"![{alt}]({url})", 1)
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(alt, TextType.IMAGE, url))
+            left_over_text = sections[1]
+        
+        if left_over_text != "":
+            new_nodes.append(TextNode(left_over_text, TextType.TEXT))
+        
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        link_list = extract_markdown_links(node.text)
+
+        if len(link_list) == 0:
+            new_nodes.append(TextNode(node.text, node.text_type))
+            continue
+        
+        sections = []
+        left_over_text = node.text
+        for alt, url in link_list:
+            sections = left_over_text.split(f"[{alt}]({url})", 1)
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            new_nodes.append(TextNode(alt, TextType.LINK, url))
+            left_over_text = sections[1]
+
+        if left_over_text != "":
+            new_nodes.append(TextNode(left_over_text, TextType.TEXT))
+        
+    return new_nodes
+
+
