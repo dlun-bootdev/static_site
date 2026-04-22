32a38d7bdea19cbcf987692a6fff35424919538a
dlun-bootdev <dluntemp-bootdev@yahoo.com>
2026-04-23 00:14:49 +0800

    code finally

--- src/test_sub_functions.py
+++ src/test_sub_functions.py
@@ -1,5 +1,11 @@
 import unittest
-from sub_functions import extract_markdown_images, extract_markdown_links
+from textnode import TextNode, TextType, text_node_to_html_node
+from sub_functions import \
+    extract_markdown_images, \
+    extract_markdown_links, \
+    split_nodes_image, \
+    split_nodes_link, \
+    markdown_to_blocks
 
 
 class TestExtractMarkdownImage(unittest.TestCase):
@@ -16,3 +22,62 @@ class TestExtractMarkdownImage(unittest.TestCase):
         self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
 
 
+class TestSplitImage(unittest.TestCase):
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+
+class TestSplitLink(unittest.TestCase):
+    def test_split_link(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a link ", TextType.TEXT),
+                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode(
+                    "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+                ),
+            ],
+            new_nodes,
+        )
+
+class TestMarkdownToBlocks(unittest.TestCase):
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
--- src/textnode.py
+++ src/textnode.py
@@ -10,6 +10,14 @@ class TextType(Enum):
     LINK = "link"
     IMAGE = "image"
 
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 class TextNode():
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -24,7 +32,7 @@ class TextNode():
         )
 
     def __repr__(self):
-        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
     
 def text_node_to_html_node(text_node):
     match text_node.text_type:
@@ -41,4 +49,35 @@ def text_node_to_html_node(text_node):
         case TextType.IMAGE:
             return LeafNode("img", "", {"src":text_node.url, "alt":text_node.text})
         case _:
-            raise ValueError("Text node not found")    
+            raise ValueError("Text node not found")
+        
+def block_to_block_type(block):
+    for i in range(7):
+        if block[i] == "#":
+            continue
+        if block[i] == " ":
+            if i > 0:
+                return BlockType.HEADING
+        break
+    
+    if block[:4] == "```\n" and block[-3:] == "```":
+        return BlockType.CODE
+    
+    lines = block.split("\n")
+    if all(line.startswith(">") for line in lines):
+        return BlockType.QUOTE
+    if all(line.startswith("- ") for line in lines):
+        return BlockType.UNORDERED_LIST
+    
+    i = 1
+    for line in lines:
+        if line.startswith(f"{i}. "):
+            i += 1
+        else:
+            break
+        if i == len(lines) + 1:
+            return BlockType.ORDERED_LIST
+    
+    return BlockType.PARAGRAPH
+    
+
